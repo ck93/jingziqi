37eabeff259f8063b1e6c3e4c32d76e5be935423
ck93 <chenkaiwelcome@gmail.com>
2013-10-01 01:52:15 +0800

    实现基本的双人对战

--- jingziqi/Form1.Designer.cs
+++ jingziqi/Form1.Designer.cs
@@ -33,13 +33,20 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(40, 31);
+            this.pictureBox1.Location = new System.Drawing.Point(34, 31);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(242, 242);
             this.pictureBox1.TabIndex = 0;
@@ -77,6 +84,7 @@
             this.groupBox1.BackColor = System.Drawing.Color.Transparent;
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.groupBox1.Location = new System.Drawing.Point(304, 31);
             this.groupBox1.Name = "groupBox1";
@@ -85,6 +93,79 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "选择颜色";
             // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(353, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "重新开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.Items.AddRange(new object[] {
+            "双人对战",
+            "人机对战"});
+            this.comboBox1.Location = new System.Drawing.Point(353, 145);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(89, 25);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(291, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "模式选择";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(305, 85);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(137, 39);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "选择顺序";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(13, 16);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 21);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "先手";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(76, 16);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(50, 21);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.Text = "后手";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -92,6 +173,10 @@
             this.BackColor = System.Drawing.SystemColors.Control;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(455, 322);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
@@ -99,7 +184,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -109,6 +197,12 @@
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
 
     }
 }
--- jingziqi/Form1.cs
+++ jingziqi/Form1.cs
@@ -14,10 +14,13 @@ namespace jingziqi
         public Form1()
         {
             InitializeComponent();
-            
+            comboBox1.SelectedIndex = 0;
         }
+        int[,] array = new int[3,3]{{0,0,0},{0,0,0},{0,0,0}};
         private enum mycolor{black, white }
         mycolor chess;
+        bool first = true;
+        int step = 1;
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics; //创建画板,这里的画板是由Form提供的.
@@ -28,15 +31,28 @@ namespace jingziqi
             g.DrawLine(blackpen, 80, 1, 80, 240);
             g.DrawLine(blackpen, 160, 1, 160, 240);
         }
-        private void placechess(mycolor chess, Bitmap bitmap, int x, int y, int size)
+        private int victory()
         {
-            switch (chess)
+            for (int i = 0; i < 3; i++)
             {
-                case mycolor.black:
-                    break;
-                case mycolor.white:
-                    break;
+                if (array[i, 0] + array[i, 1] + array[i, 2] == 3)
+                    return 1;
+                else if (array[i, 0] + array[i, 1] + array[i, 2] == -3)
+                    return -1;
+                if (array[0, i] + array[1, i] + array[2, i] == 3)
+                    return 1;
+                else if (array[0, i] + array[1, i] + array[2, i] == -3)
+                    return -1;
             }
+            if (array[0, 0] + array[1, 1] + array[2, 2] == 3)
+                return 1;
+            else if (array[0, 0] + array[1, 1] + array[2, 2] == -3)
+                return -1;
+            if (array[0, 2] + array[1, 1] + array[2, 0] == 3)
+                return 1;
+            else if (array[0, 2] + array[1, 1] + array[2, 0] == -3)
+                return -1;
+            return 0;
         }
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
@@ -45,8 +61,40 @@ namespace jingziqi
             Graphics g = pictureBox1.CreateGraphics();
             Bitmap black = new Bitmap(@"D:\Documents\GitHub\jingziqi\jingziqi\黑子.png");
             Bitmap white = new Bitmap(@"D:\Documents\GitHub\jingziqi\jingziqi\白子.png");
-
-            
+            if (chess == mycolor.black)
+            {
+                if (first && step % 2 == 1 || !first && step % 2 == 0)
+                {
+                    g.DrawImage(black, x / 80 * 80 + 10, y / 80 * 80 + 10, 60, 60);
+                    array[x / 80, y / 80] = 1;
+                }
+                else
+                {
+                    g.DrawImage(white, x / 80 * 80 + 10, y / 80 * 80 + 10, 60, 60);
+                    array[x / 80, y / 80] = -1;
+                }
+                step++;
+            }
+            else
+            {
+                if (first && step % 2 == 1 || !first && step % 2 == 0)
+                {
+                    g.DrawImage(white, x / 80 * 80 + 10, y / 80 * 80 + 10, 60, 60);
+                    array[x / 80, y / 80] = -1;
+                }
+                else
+                {
+                    g.DrawImage(black, x / 80 * 80 + 10, y / 80 * 80 + 10, 60, 60);
+                    array[x / 80, y / 80] = 1;
+                }
+                step++;
+            }
+            if (victory() == 1)
+                MessageBox.Show("黑子赢！");
+            if (victory() == -1)
+                MessageBox.Show("白子赢！");
+            if (step == 10)
+                MessageBox.Show("和棋！");
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
@@ -56,22 +104,26 @@ namespace jingziqi
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
-            chess = mycolor.black;
+            chess = mycolor.white;
         }
-        /*private void Form1_Paint(object sender, PaintEventArgs e)
+
+        private void button1_Click(object sender, EventArgs e)
         {
-            Graphics g = e.Graphics; //创建画板,这里的画板是由Form提供的.
-            Pen blackpen = new Pen(Color.Black, 2);//定义了一个黑色,宽度为的画笔
-            g.DrawRectangle(blackpen, 59, 29, 246, 246);//在画板上画矩形,起始坐标为(10,10),宽为,高为
-            g.DrawLine(blackpen, 59, 111, 305, 111);//在画板上画直线,起始坐标为(10,10),终点坐标为(100,100)
-            g.DrawLine(blackpen, 59, 193, 305, 193);
-            g.DrawLine(blackpen, 141, 29, 141, 275);
-            g.DrawLine(blackpen, 223, 29, 223, 275);
-        }*/
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    array[i,j] = 0;
+            pictureBox1.Image = null;
+            step = 0;
+        }
 
-       
-    
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            first = true;
+        }
 
-
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            first = false;
+        }
     }
 }
