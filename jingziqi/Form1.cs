39d54c54e74b22821a3895cb81c88bdf93fd2e34
ck93 <chenkaiwelcome@gmail.com>
2013-09-30 19:44:17 +0800

    九个picturebox

---
+++ jingziqi/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace jingziqi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; //创建画板,这里的画板是由Form提供的.
+            Pen blackpen = new Pen(Color.Black, 2);//定义了一个黑色,宽度为的画笔
+            g.DrawRectangle(blackpen, 59, 29, 246, 246);//在画板上画矩形,起始坐标为(10,10),宽为,高为
+            g.DrawLine(blackpen, 59, 111, 305, 111);//在画板上画直线,起始坐标为(10,10),终点坐标为(100,100)
+            g.DrawLine(blackpen, 59, 193, 305, 193);
+            g.DrawLine(blackpen, 141, 29, 141, 275);
+            g.DrawLine(blackpen, 223, 29, 223, 275);
+        }
+
+       
+    
+
+
+    }
+}
